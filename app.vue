f902814e55122b86f6795691caa8b72fae262f8f
Aleksi <liukkonenaleksi@gmail.com>
2024-12-30 14:20:51 +0200

    Add prettier

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div class="p-8 font-sans text-7xl">Hello Nuxt {{ version }}!</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   typescript: {
-    typeCheck: true,
+    typeCheck: true
   },
-  modules: ['@nuxtjs/tailwindcss'],
-});
+  modules: ['@nuxtjs/tailwindcss']
+})
