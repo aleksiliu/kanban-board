f581dda3f98f5dfae209c343ce05b459956e9c71
Aleksi <liukkonenaleksi@gmail.com>
2025-01-03 11:43:59 +0200

    Edit green

--- components/common/Notification.vue
+++ components/common/Notification.vue
@@ -16,7 +16,7 @@
     <div
       v-if="isVisible"
       class="fixed bottom-4 left-1/2 z-50 -translate-x-1/2 rounded-lg px-4 py-2 text-sm text-white shadow-lg"
-      :class="notification?.type === 'success' ? 'bg-green-500' : 'bg-red-500'"
+      :class="notification?.type === 'success' ? 'bg-green-400' : 'bg-red-500'"
     >
       {{ notification?.message }}
     </div>
